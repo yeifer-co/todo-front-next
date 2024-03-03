7c54b1cfbe0fb0e2fec68f4ab24e8d69c15ec251
Yeifer Muñoz <yeifer.munoz@telintel.net>
2024-03-03 10:12:17 -0500

    style(task): :lipstick: Add title and font-awesome stylesheet to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 
 import { Provider } from 'react-redux'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import store from '../app/store'
 import Sidebar from '../components/sidebar/Sidebar'
@@ -11,10 +12,21 @@ import ContextProvider from '../providers/ContextProvider'
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Head>
+        <title>Autority Challenge</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </Head>
       <ContextProvider>
         <GlobalStyleProvider>
           <Sidebar></Sidebar>
           <Component {...pageProps} />
+          content
         </GlobalStyleProvider>
       </ContextProvider>
     </Provider>
